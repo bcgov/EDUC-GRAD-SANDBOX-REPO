037541cf4680dac762663573991f460800f60521
Kamal Mohammed <kamal.mohammed@outlook.com>
2021-01-19 11:08:22 -0800

    Reading file using input stream

--- api/src/main/java/ca/bc/gov/gradsandbox/service/SandboxService.java
+++ api/src/main/java/ca/bc/gov/gradsandbox/service/SandboxService.java
@@ -2,18 +2,20 @@ package ca.bc.gov.gradsandbox.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
+//import org.springframework.core.io.Resource;
+//import org.springframework.core.io.ResourceLoader;
+
+import java.io.*;
+import java.util.stream.Collectors;
 
 @Service
 public class SandboxService {
 
-    @Value("classpath:templates/student_achievement_report_template.docx")
-    Resource reportTemplateResource;
+    //@Autowired
+    //ResourceLoader resourceLoader;
 
     private static Logger logger = LoggerFactory.getLogger(SandboxService.class);
 
@@ -23,8 +25,19 @@ public class SandboxService {
 
     public String readFileResource() throws IOException {
         logger.debug("Reading File");
-        File templateFile = reportTemplateResource.getFile();
-        logger.debug("File Path: " + templateFile.getPath());
-        return "File Read Successful! Location: " + templateFile.getPath() ;
+        String fileContent = "";
+
+        //Resource reportTemplateResource=resourceLoader.getResource("classpath:templates/student_achievement_report_template.docx");
+        //File templateFile = reportTemplateResource.getFile();
+
+        //*************************************
+        try (InputStream inputStream = getClass().getResourceAsStream("/templates/student_achievement_report_template.docx");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            fileContent = reader.lines()
+                    .collect(Collectors.joining(System.lineSeparator()));
+        }
+
+        logger.debug("File Content: \n" + fileContent);
+        return "File Read Successful! : \n" + fileContent ;
     }
 }
