b94dadd87e5c6275fcd644de3ba17f7614d6a8a4
Kamal Mohammed <kamal.mohammed@outlook.com>
2021-01-19 08:12:13 -0800

    Added code to Read File

--- api/src/main/java/ca/bc/gov/gradsandbox/controller/SandboxController.java
+++ api/src/main/java/ca/bc/gov/gradsandbox/controller/SandboxController.java
@@ -6,15 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
-@RequestMapping("/api/v1/get-data")
+@RequestMapping("/api/v1")
 public class SandboxController {
 
     @Autowired
     SandboxService sandboxService;
 
-    @GetMapping
+    @GetMapping("/get-data")
     public String getStudentCourses() {
         return sandboxService.getData();
+    }
+
+    @GetMapping("/read-template")
+    public String readFileResource() {
+        try {
+            return sandboxService.readFileResource();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error reading file: " + e.getMessage();
+        }
     }
 }
--- api/src/main/java/ca/bc/gov/gradsandbox/service/SandboxService.java
+++ api/src/main/java/ca/bc/gov/gradsandbox/service/SandboxService.java
@@ -1,11 +1,30 @@
 package ca.bc.gov.gradsandbox.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
 
 @Service
 public class SandboxService {
 
+    @Value("classpath:templates/student_achievement_report_template.docx")
+    Resource reportTemplateResource;
+
+    private static Logger logger = LoggerFactory.getLogger(SandboxService.class);
+
     public String getData() {
         return "Data from Sandbox Service";
+    }
+
+    public String readFileResource() throws IOException {
+        logger.debug("Reading File");
+        File templateFile = reportTemplateResource.getFile();
+        logger.debug("File Path: " + templateFile.getPath());
+        return "File Read Successful! Location: " + templateFile.getPath() ;
     }
 }
