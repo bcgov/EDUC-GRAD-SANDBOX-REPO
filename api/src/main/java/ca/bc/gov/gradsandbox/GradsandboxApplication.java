a4260193bd602a667cc7b6f7e5d732788999a78a
Kamal <kamal.mohammed@outlook.com>
2022-03-08 16:09:55 -0700

    Cleanup

---
+++ api/src/main/java/ca/bc/gov/gradsandbox/GradsandboxApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.gradsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradsandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradsandboxApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/ca/bc/gov/gradsandbox/GradsandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.gradsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradsandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
