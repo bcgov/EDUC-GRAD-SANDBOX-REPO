d2ada38f397399eda69903b621537d8349f7628b
Kamal <kamal.mohammed@outlook.com>
2022-03-08 16:09:15 -0700

    Cleanup

--- api/src/main/java/ca/bc/gov/gradsandbox/repository/SandboxRepository.java
+++
@@ -1,9 +0,0 @@
-package ca.bc.gov.gradsandbox.repository;
-
-import ca.bc.gov.gradsandbox.entity.SandboxEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SandboxRepository extends CrudRepository<SandboxEntity, String> {
-}
