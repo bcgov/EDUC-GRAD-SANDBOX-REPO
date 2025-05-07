523cb568e9931ab5404088872456848aa242202e
Kamal Mohammed <kamal.mohammed@outlook.com>
2025-05-07 11:13:27 +0530

    Update fetch-and-create-secrets.js

--- tools/openshift/fetch-and-create-secrets.js
+++ tools/openshift/fetch-and-create-secrets.js
@@ -50,8 +50,8 @@ async function createK8sSecret({ clientId, secret }) {
   };
 
   const secretName = `${clientId}-secret`;
-  const clientIdKey = `${clientId}-NAME`.toUpperCase.replaceAll('-', '_');
-  const clientSecretKey = `${clientId}-SECRET`.toUpperCase.replaceAll('-', '_');
+  const clientIdKey = `${clientId}-NAME`.toUpperCase().replaceAll('-', '_');
+  const clientSecretKey = `${clientId}-SECRET`.toUpperCase().replaceAll('-', '_');
 
   const payload = {
     apiVersion: 'v1',
